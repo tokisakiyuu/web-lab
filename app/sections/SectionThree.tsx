42d108e9a1efedf6ff249e955a8511853b731baf
tokisakiyuu <tokisakiyuu@qq.com>
2024-12-10 21:35:09 +0800

    update

--- app/sections/SectionThree.tsx
+++ app/sections/SectionThree.tsx
@@ -1,9 +1,10 @@
 const SectionThree = () => {
   return (
     <section className="bg-background-color h-[100vh] w-[100vw] flex-shrink-0 flex">
-      <h1 className="text-primary-color text-2xl m-auto">Section Three</h1>
+      <h1 className="text-primary-color text-2xl m-auto">你说放点啥好捏</h1>
     </section>
-  )
-}
+  );
+};
 
-export default SectionThree
+export default SectionThree;
+
